eef3d01ff6c2488044040db55ea20872a1cadbdc
yeti-detective <yeti.detective@gmail.com>
2017-02-13 19:22:54 -0500

    ok ok ok

--- src/client/app/minigame/Controller.jsx
+++ src/client/app/minigame/Controller.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 var triangle = "4em solid transparent";
-var hyp = "8em solid #15316D";
+var hyp = "8em solid #082156";
 
 var style = {
     width: "100%",
@@ -29,7 +29,7 @@ var right = {
 var jump = {
     width: "8em",
     height: "8em",
-    backgroundColor: "#15316D",
+    backgroundColor: "#082156",
     display: "inline-block",
     margin: "0px 20% 0px 20%",
     borderRadius: "50%"
