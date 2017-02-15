f0b517c2d87e818c03c0295653959516a9f9db3c
Christopher M Brown <yeti.detective@gmail.com>
2017-02-15 16:21:40 +0000

    a better explanation

--- src/client/app/Explaining.jsx
+++ src/client/app/Explaining.jsx
@@ -4,9 +4,8 @@ var Explaining = React.createClass({
     render: function(){
         return(
             <div>
-                <h3>Like Me!</h3>
-                <p>Clicking a button and seeing a response gives you a dopamine hit.</p>
-                <p>I am not a drug dealer.</p>
+                <h3>Like My Beard!</h3>
+                <br />
                 <p>I built this page with React and host it from a Node server which I also wrote.</p>
                 <p>You could say I'm pretty good at JavaScript.</p>
                 <p>But you don't have to take MY word for it.</p>
