1ac3c11b7aff2516a7e2059e75413a55db015672
Christopher M Brown <yeti.detective@gmail.com>
2017-02-15 03:10:11 +0000

    FOR LOVE

--- src/client/app/Explaining.jsx
+++ src/client/app/Explaining.jsx
@@ -4,13 +4,15 @@ var Explaining = React.createClass({
     render: function(){
         return(
             <div>
-                <h3>Like My Rad Coat</h3>
+                <h3>Like Me!</h3>
+                <p>Clicking a button and seeing a response gives you a dopamine hit.</p>
+                <p>I am not a drug dealer.</p>
                 <p>I built this page with React and host it from a Node server which I also wrote.</p>
                 <p>You could say I'm pretty good at JavaScript.</p>
                 <p>But you don't have to take MY word for it.</p>
                 <p>Click on these links to adore my work</p>
             </div>
-        )
+        );
     }
 });
 
