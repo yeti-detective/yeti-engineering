6bcdf3f8f2b35a699ee54e19b9f3c5953d60e939
yeti-detective <yeti.detective@gmail.com>
2017-02-14 20:35:10 -0500

    Slight improvements

--- src/client/app/Explaining.jsx
+++ src/client/app/Explaining.jsx
@@ -4,10 +4,11 @@ var Explaining = React.createClass({
     render: function(){
         return(
             <div>
-                <h3>Like My Face</h3>
+                <h3>Like My Rad Coat</h3>
                 <p>I built this page with React and host it from a Node server which I also wrote.</p>
                 <p>You could say I'm pretty good at JavaScript.</p>
                 <p>But you don't have to take MY word for it.</p>
+                <p>Click on these links to adore my work</p>
             </div>
         )
     }
