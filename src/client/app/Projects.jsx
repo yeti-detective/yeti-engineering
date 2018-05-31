677f9de71b1ede46564006fa427f9352c5f4e53e
yeti-detective <yeti.detective@gmail.com>
2018-05-31 16:56:33 -0400

    refactoring to contain projects section in its own area

---
+++ src/client/app/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import JustFollowingUp from './JustFollowingUp.jsx'
+import SoundClone from './SoundClone.jsx'
+import YetiFlashlight from './YetiFlashlight.jsx'
+
+const Projects = () => (
+  <section className="projects">
+    <h1>Portfolio</h1>
+    <SoundClone />
+  </section>
+)
+
+export default Projects
