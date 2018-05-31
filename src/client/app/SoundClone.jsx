c4876fc70aaf8fde6d1353818cd7da6b05a81a4c
yeti-detective <yeti.detective@gmail.com>
2018-05-31 18:01:39 -0400

    better description of project. It's called CONTENT, Jeffrey

--- src/client/app/SoundClone.jsx
+++ src/client/app/SoundClone.jsx
@@ -2,17 +2,48 @@ import React from 'react';
 
 const SoundClone = props => (
     <div>
-        <h2>I Learned Ruby!</h2>
-        <p>In March 2018 I attended an exclusive NYC code bootcamp & learned Ruby, Rails, Postgresql, and other skills. Get at me.</p>
-        <p>Check out my full-stack project, Rails backend, React frontend. It's a clone of <a href="https://www.soundcloud.com" target="_blank">SoundCloud</a></p>
-        <p>I call it <a href="https://chris-b-soundclone.herokuapp.com" target="_blank">SoundClone</a> (not very original, but it's too late to change it now.)</p>
-        <a href="https://chris-b-soundclone.herokuapp.com" target="_blank"><img 
+        <h2>
+          <a href="https://chris-b-soundclone.herokuapp.com">SoundClone</a>
+        </h2>
+        <a href="https://chris-b-soundclone.herokuapp.com" target="_blank">
+          <img
             style={{width: '80%', height: 'auto'}}
-            src="https://dl.dropbox.com/s/548urphtr7gly2e/20180518LandingPage.png?dl=0" 
-            title="screenshot of landing page of soundclone" 
-        /></a>
-        <p>GitHub repo <a href="https://github.com/yeti-detective/SoundClone" target="_blank">here</a></p>
-    </div>    
+            src="/images/SoundClone.png"
+            title="screenshot of landing page of soundclone"
+          />
+        </a>
+        <div className="project-explanation">
+          <p>
+            SoundClone, inspired by <a href="https://www.soundcloud.com/" target="_blank">
+            SoundCloud</a>, is a full stack app I developed as a student project
+            at <a href="https://appacademy.io" target="_blank">App Academy</a>
+            NYC.
+          </p>
+          <p>
+            The backend uses Ruby on Rails to deploy a PostgreSQL database integrated
+            with an AWS S3 instance for media attachments, a Puma server handles
+            the HTTP traffic, and Rails' domain specific language Jbuilder creates
+            the JSON views that power the app's API.
+          </p>
+          <p>
+            User authentication is managed by Ruby's BCrypt library. Password digests
+            (hashed and salted representations of the user's password) are generated
+            with the Blowfish encryption algorithm and associated with the browser
+            session token sent with each HTTP request. When a user logs out, a
+            random token is stored in their browser, and the associated token
+            in the database is set to a nil value.
+          </p>
+          <p>
+            On the frontend, user experience is enhanced by React. An HTML document
+            is sent only on the initial HTTP request. Components are rendered from
+            JavaScript classes, and application state is tracked in a Redux store.
+            Every component is able to fetch its required data from the backend API
+            using Ajax requests, and meaninful URLs are generated and interpreted
+            by React-Router so a user can post a link to a specific view and the
+            same view will render for other users who click that link.
+          </p>
+        </div>
+    </div>
 );
 
 export default SoundClone;
