40d8a03b8c8ece88857b807c1b0c5ac8688896c0
yeti-detective <yeti.detective@gmail.com>
2018-05-31 12:50:09 -0400

    updated babel, webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,23 +4,23 @@ var path = require('path');
 var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
-var config = {
+module.exports = {
     entry: APP_DIR + '/index.jsx',
     output: {
         path: BUILD_DIR,
         filename: 'bundle.js'
     },
     module: {
-        loaders: [
+        rules: [
             {
-                test: /\.jsx?/,
+                test: [/\.jsx?/, /\.js?$/],
                 include: APP_DIR,
-                loader: 'babel'
-            },
-            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                  presets: ['env', 'react']
+                }
+            }
         ]
     }
 };
-
-module.exports = config;
