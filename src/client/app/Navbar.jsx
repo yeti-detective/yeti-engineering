39facf8bdbe46f9b995caaaf214b302d473429d2
yeti-detective <yeti.detective@gmail.com>
2018-05-31 16:13:40 -0400

    improved code by removing JS objects & styling components in CSS

--- src/client/app/Navbar.jsx
+++ src/client/app/Navbar.jsx
@@ -1,34 +1,12 @@
 import React from 'react';
 
-var style = {
-    backgroundColor: "#070A16",
-    width: "100%",
-    margin: 0,
-    padding: 0,
-    overflow: "hidden",
-    position: "fixed",
-    top: 0,
-    left: 0,
-    textAlign: "left",
-    display: "flex"
-};
-
-var headers = {
-    display: "inline-block",
-    margin: "auto"
-};
-
-var link = {
-    color: "#3F5A95"
-}
-
 const Navbar = () => {
   return (
-    <div style={style}>
-      <h2 style={headers}>Christopher Brown</h2>
-      <h3 style={headers}>Software Developer</h3>
-      <h3 style={headers}>
-        <a href="tel:8166592363" style={link}>call me</a>
+    <div className={'header-bar'}>
+      <h2>Christopher Brown</h2>
+      <h3>Software Developer</h3>
+      <h3>
+        <a href="tel:8166592363">call me</a>
       </h3>
     </div>
   );
