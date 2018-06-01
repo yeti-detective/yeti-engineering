51223475157e686bb0277f0b3db871680008d7c8
yeti-detective <yeti.detective@gmail.com>
2018-06-01 12:22:30 -0400

    added RubyChess component

--- src/client/app/Projects.jsx
+++ src/client/app/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import JustFollowingUp from './projects/JustFollowingUp.jsx'
+import RubyChess from './projects/RubyChess.jsx'
 import SoundClone from './projects/SoundClone.jsx'
 import Sanik from './projects/Sanik.jsx'
 import YetiFlashlight from './projects/YetiFlashlight.jsx'
@@ -11,6 +12,7 @@ const Projects = () => (
     <div className="projects-grid">
       <SoundClone />
       <Sanik />
+      <RubyChess />
       <JustFollowingUp />
       <YetiFlashlight />
     </div>
---
+++ src/client/app/projects/RubyChess.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const RubyChess = () => (
+  <section className="ruby-chess">
+    <h2>
+      <a
+        href="https://github.com/yeti-detective/RubyChess"
+        target="_blank"
+        >
+        Ruby Chess
+      </a>
+    </h2>
+    <div className="ruby-chess-screenshots">
+      <a
+        href="https://github.com/yeti-detective/RubyChess"
+        target="_blank"
+        >
+        <img src="/images/chess_board.png"/>
+        <img src="/images/chess_board2.png"/>
+      </a>
+    </div>
+    <div className="ruby-chess-words">
+      <p>
+        This is a pure-ruby project <em>(still in development.)</em> The goal
+        is to create a computer chess opponent that models a tree data structure
+        where the first generation of leaves is each possible move it could
+        execute from the current state, the next generation is every possible
+        response the opponent could produce, and so on.
+      </p>
+      <p>
+        While creating this tree structure, it will
+        use <a href="https://en.wikipedia.org/wiki/Breadth-first_search"
+        target="_blank">depth-first search</a> to find the shortest branch that
+        ends in victory, and then pursue the course of action defined by that
+        branch.
+      </p>
+      <p>
+        I hope to gain some insight into AI in the pursuit of this project.
+        Watch this space for updates.
+      </p>
+    </div>
+  </section>
+)
+
+export default RubyChess
