3e82a5d306e668fe115b2c4447c764ef96593043
yeti-detective <yeti.detective@gmail.com>
2018-06-01 12:56:54 -0400

    moved style from js objects to css

--- src/client/app/Links.jsx
+++ src/client/app/Links.jsx
@@ -1,36 +1,26 @@
 import React from 'react';
 
-var style = {
-  color: "#264586",
-  margin: "5%",
-  fontSize: "2.5em"
-};
-
-// <a href="https://www.codewars.com/users/yeti-detective"><i className="fa fa-lg fa-codewars" style={style}></i></a>
-// <a href="https://heroku.com/insert-link-to-cool-app-here"><i className="fa fa-lg fa-heroku" style={style}></i></a>
-// my kingdom for a codewars or heroku fontawesome icon
-
 const Links = () => (
   <div>
     <a href="https://codepen.io/Yeti_Detective"
       target="_blank">
-      <i className="fab fa-lg fa-codepen" style={style} ></i>
+      <i className="link fab fa-lg fa-codepen"></i>
     </a>
     <a href="https://www.freecodecamp.com/yeti-detective/front-end-certification"
       target="_blank">
-      <i className="fab fa-lg fa-free-code-camp" style={style}></i>
+      <i className="link fab fa-lg fa-free-code-camp"></i>
     </a>
     <a href="https://github.com/yeti-detective"
       target="_blank">
-      <i className="fab fa-lg fa-github" style={style}></i>
+      <i className="link fab fa-lg fa-github"></i>
     </a>
     <a href="https://www.linkedin.com/in/xchristopherxbrown"
       target="_blank">
-      <i className="fab fa-lg fa-linkedin-in" style={style}></i>
+      <i className="link fab fa-lg fa-linkedin-in"></i>
     </a>
     <a href="mailto:christopher.brown@motomorphosis-ind.com"
       target="_blank">
-      <i className="fa fa-lg fa-envelope" style={style}></i>
+      <i className="link fa fa-lg fa-envelope"></i>
     </a>
   </div>
   );
