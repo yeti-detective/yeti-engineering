717d55ec8f0bd65a3e5de6a4f58beb361170d737
yeti-detective <yeti.detective@gmail.com>
2018-06-01 12:54:02 -0400

    additional facts

---
+++ src/client/app/AdditionalFacts.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const AdditionalFacts = () => (
+  <section className="additional-facts">
+    <div className="about-me">
+      I am just a guy. I was born in Frankfurt am main, Germany. Moved to the US
+      as a wee baby. Grew up in the Midwest. Moved to New York at the beginning
+      of 2017, and I love it here.
+    </div>
+    <div className="Motomorphosis Industries">
+      Motomorphosis is my company. Right now the only thing it sells is me.
+      I have plans for it, though. Keep an eye out.
+    </div>
+  </section>
+)
+
+export default AdditionalFacts
