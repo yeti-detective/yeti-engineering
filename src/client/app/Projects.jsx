1fe6244647b4d8fcb48d2dfc3ffa16a56dc95f47
yeti-detective <yeti.detective@gmail.com>
2018-06-01 10:46:11 -0400

    project components moved to parent Projects component"

--- src/client/app/Projects.jsx
+++ src/client/app/Projects.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import JustFollowingUp from './JustFollowingUp.jsx'
 import SoundClone from './SoundClone.jsx'
 import YetiFlashlight from './YetiFlashlight.jsx'
+import Sanik from './Sanik.jsx'
 
 const Projects = () => (
   <section className="projects">
     <h1>Portfolio</h1>
+    <hr />
     <div className="projects-grid">
       <SoundClone />
+      <Sanik />
+      <JustFollowingUp />
     </div>
   </section>
 )
