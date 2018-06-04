bde48469d0d3dace4b8d15abf0eed74e1403627f
yeti-detective <yeti.detective@gmail.com>
2018-06-04 11:48:31 -0400

    aria-labels for accessibility

--- src/client/app/Links.jsx
+++ src/client/app/Links.jsx
@@ -4,32 +4,32 @@ const Links = () => (
   <div>
     <a href="https://codepen.io/Yeti_Detective"
       target="_blank"
-      title="my CodePen profile">
+      aria-label="my CodePen profile">
       <i className="link fab fa-lg fa-codepen"></i>
     </a>
     <a href="https://www.freecodecamp.org/certification/yeti-detective/legacy-front-end"
       target="_blank"
-      title="frontend certification">
+      aria-label="frontend certification">
       <i className="link fab fa-lg fa-free-code-camp"></i>
     </a>
     <a href="https://github.com/yeti-detective"
       target="_blank"
-      title="link to my GitHub">
+      aria-label="link to my GitHub">
       <i className="link fab fa-lg fa-github"></i>
     </a>
     <a href="https://www.linkedin.com/in/xchristopherxbrown"
       target="_blank"
-      title="LinkedIn">
+      aria-label="LinkedIn">
       <i className="link fab fa-lg fa-linkedin-in"></i>
     </a>
     <a href="mailto:christopher.brown@motomorphosis-ind.com"
       target="_blank"
-      title="email me">
+      aria-label="email me">
       <i className="link fa fa-lg fa-envelope"></i>
     </a>
     <a href="https://www.freecodecamp.org/certification/yeti-detective/javascript-algorithms-and-data-structures"
     target="_blank"
-    title="JS algorithms & data structures certification">
+    aria-label="JS algorithms & data structures certification">
     <i className="link fab fa-lg fa-free-code-camp"></i>
   </a>
   </div>
