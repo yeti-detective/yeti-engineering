c0c29a24f5f2ddb26827e488723d31da3b580bfa
yeti-detective <yeti.detective@gmail.com>
2018-06-04 10:45:03 -0400

    adding title text for accessibility, but just found out that screen readers usually ignore it. Awright

--- src/client/app/Links.jsx
+++ src/client/app/Links.jsx
@@ -3,25 +3,35 @@ import React from 'react';
 const Links = () => (
   <div>
     <a href="https://codepen.io/Yeti_Detective"
-      target="_blank">
+      target="_blank"
+      title="my CodePen profile">
       <i className="link fab fa-lg fa-codepen"></i>
     </a>
     <a href="https://www.freecodecamp.org/certification/yeti-detective/legacy-front-end"
-      target="_blank">
+      target="_blank"
+      title="frontend certification">
       <i className="link fab fa-lg fa-free-code-camp"></i>
     </a>
     <a href="https://github.com/yeti-detective"
-      target="_blank">
+      target="_blank"
+      title="link to my GitHub">
       <i className="link fab fa-lg fa-github"></i>
     </a>
     <a href="https://www.linkedin.com/in/xchristopherxbrown"
-      target="_blank">
+      target="_blank"
+      title="LinkedIn">
       <i className="link fab fa-lg fa-linkedin-in"></i>
     </a>
     <a href="mailto:christopher.brown@motomorphosis-ind.com"
-      target="_blank">
+      target="_blank"
+      title="email me">
       <i className="link fa fa-lg fa-envelope"></i>
     </a>
+    <a href="https://www.freecodecamp.org/certification/yeti-detective/javascript-algorithms-and-data-structures"
+    target="_blank"
+    title="JS algorithms & data structures certification">
+    <i className="link fab fa-lg fa-free-code-camp"></i>
+  </a>
   </div>
   );
 
