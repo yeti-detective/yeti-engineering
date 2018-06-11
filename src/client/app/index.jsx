7b564e7b8f7d6f499fe41243c1ecb1b0a522bbd3
yeti-detective <yeti.detective@gmail.com>
2018-06-11 12:31:38 -0400

    moved some style from JS to CSS, improved style of footer component

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -10,18 +10,10 @@ import Projects from './Projects';
 import AdditionalFacts from './AdditionalFacts';
 import Footer from './Footer';
 
-
-var style = {
-    width: '100%',
-    maxWidth: 1200,
-    background: 'linear-gradient(#153162, #071F49)',
-    margin: 'auto',
-};
-
 class App extends React.Component {
     render() {
         return (
-            <div style={style}>
+            <div className="app">
                 <Navbar />
                 <Links />
                 <Intro />
