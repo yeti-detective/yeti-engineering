03c615475291bf14e25ce86cb2b971ff15ce6cf5
yeti-detective <yeti.detective@gmail.com>
2018-10-03 22:17:10 -0400

    linter did some good and added a route for /payme lol

--- server.js
+++ server.js
@@ -1,70 +1,83 @@
 let port = process.env.PORT || 8000;
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const mongo = require('mongodb').MongoClient
+const path = require("path");
+const mongo = require("mongodb").MongoClient;
 
-const validPhone = require(path.join(__dirname + '/src/client/app/scripts/verify_phone'));
+const validPhone = require(path.join(
+  __dirname + "/src/client/app/scripts/verify_phone"
+));
 
-const mongo_pw = process.env.MPW || require('./mongo_pw.js').password
+const mongo_pw = process.env.MPW || require("./mongo_pw.js").password;
 
-let db
-let likes
-let id
-debugger
+let db;
+let likes;
+let id;
+debugger;
 // serve the app
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/src/client/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/src/client/index.html"));
 });
 
-app.get('/images/:filename', (req, res) => {
-  res.sendFile(path.join(__dirname + `/src/client/public/images/${req.params.filename}`))
-})
-
-app.get('/style', (req, res) => {
-  res.sendFile(path.join(__dirname + '/src/client/public/style.css'))
-})
-
-app.get('/wedding', (req, res) =>{
-    res.sendFile(path.join(__dirname + '/src/client/wedding.html'));
+app.get("/images/:filename", (req, res) => {
+  res.sendFile(
+    path.join(__dirname + `/src/client/public/images/${req.params.filename}`)
+  );
 });
 
-app.get('/urlShortner', (req, res) =>{
-    res.sendFile(path.join(__dirname + '/src/client/urlShortener.html'));
+app.get("/style", (req, res) => {
+  res.sendFile(path.join(__dirname + "/src/client/public/style.css"));
+});
+
+app.get("/wedding", (req, res) => {
+  res.sendFile(path.join(__dirname + "/src/client/wedding.html"));
+});
+
+app.get("/urlShortner", (req, res) => {
+  res.sendFile(path.join(__dirname + "/src/client/urlShortener.html"));
 });
 
 // serve the script
-app.get('/script', (req, res) => {
-    res.sendFile(path.join(__dirname + '/src/client/public/bundle.js'));
+app.get("/script", (req, res) => {
+  res.sendFile(path.join(__dirname + "/src/client/public/bundle.js"));
 });
 
-app.get('/likes', (req, res) =>{
-    res.json(likes);
+app.get("/likes", (req, res) => {
+  res.json(likes);
 });
 
 // post likes to the hosted database
-app.post('/click', (req, res) => {
-    db.collection('likes').save({_id: 69420247, likes: ++likes})
-    res.json(likes)
+app.post("/click", (req, res) => {
+  db.collection("likes").save({ _id: 69420247, likes: ++likes });
+  res.json(likes);
 });
 
-app.get('/click', (req, res) => {
-    res.json(likes);
-})
+app.get("/click", (req, res) => {
+  res.json(likes);
+});
 
-app.get('/call/:phoneNumber', (req, res) => {
-  res.redirect(`tel:${validPhone(req.params.phoneNumber)}`)
-})
+app.get("/call/:phoneNumber", (req, res) => {
+  res.redirect(`tel:${validPhone(req.params.phoneNumber)}`);
+});
 
-mongo.connect(`mongodb://liker:${mongo_pw}@ds157278.mlab.com:57278/yetis_first_db`, (err, database) => {
-    if (err) throw err
-    db = database
-    app.listen(port, ()=>{
-    console.log('listening on ' + port);
+app.get("/payme", (req, res) => {
+  res.sendFile(path.join(__dirname, "payme.html"));
+});
+
+mongo.connect(
+  `mongodb://liker:${mongo_pw}@ds157278.mlab.com:57278/yetis_first_db`,
+  (err, database) => {
+    if (err) throw err;
+    db = database;
+    app.listen(port, () => {
+      console.log("listening on " + port);
     });
-    db.collection('likes').find().toArray((err, result) => {
-        if (err) throw err
-        likes = result[0]['likes']
-    })
-})
+    db.collection("likes")
+      .find()
+      .toArray((err, result) => {
+        if (err) throw err;
+        likes = result[0]["likes"];
+      });
+  }
+);
