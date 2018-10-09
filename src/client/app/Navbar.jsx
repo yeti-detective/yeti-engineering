4805e45fbcab698a3b12562d1aa225a4a75bcb7f
yeti-detective <yeti.detective@gmail.com>
2018-10-09 05:40:14 -0400

    text change in navbar

--- src/client/app/Navbar.jsx
+++ src/client/app/Navbar.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div className={'header-bar'}>
+    <div className={"header-bar"}>
       <h2>Christopher Brown</h2>
-      <h3>Software Developer</h3>
+      <h3>Software Engineer</h3>
       <h3>
         <a href="tel:8166592363">call me</a>
       </h3>
     </div>
   );
-}
+};
 
 export default Navbar;
