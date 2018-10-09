54420e942bdcbc783c1b7a3ef2db3783bce4c51e
yeti-detective <yeti.detective@gmail.com>
2018-10-09 06:06:14 -0400

    changed image and formatted code with eslint

--- src/client/app/LikeButton.jsx
+++ src/client/app/LikeButton.jsx
@@ -1,70 +1,70 @@
-import React from 'react';
+import React from "react";
 
-class LikeButton extends React.Component{
-  constructor(){
+class LikeButton extends React.Component {
+  constructor() {
     super();
     this.state = {
       likesCount: 0
     };
-    this.upLike = this.upLike.bind(this)
-    this.onLike = this.onLike.bind(this)
+    this.upLike = this.upLike.bind(this);
+    this.onLike = this.onLike.bind(this);
   }
 
-  componentWillMount () {
-    let xhr = new XMLHttpRequest()
-    xhr.open('GET', './click')
+  componentWillMount() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "./click");
     xhr.onreadystatechange = () => {
       if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
         this.setState({
           likesCount: xhr.responseText
-        })
+        });
       }
-    }
-    xhr.send()
+    };
+    xhr.send();
   }
 
-  upLike (like) {
+  upLike(like) {
     this.setState({
       likeCount: like
-    })
+    });
   }
 
-  onLike () {
-    let xhr = new XMLHttpRequest()
-    xhr.open('POST', './click')
+  onLike() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "./click");
     xhr.onreadystatechange = () => {
       if (xhr.readyState === 4 && xhr.status === 200) {
         this.setState({
           likesCount: xhr.responseText
-        })
+        });
       }
-    }
-    xhr.send()
+    };
+    xhr.send();
   }
 
-  render(){
-    return(
-    <div onClick={this.onLike}>
-      <img
-        className="beard"
-        alt="photo of a handsome man"
-        src="/images/TrainSquint.jpg"
+  render() {
+    return (
+      <div onClick={this.onLike}>
+        <img
+          className="beard"
+          alt="photo of a handsome man"
+          src="/images/HandsomeSmartboy.jpeg"
         />
-      <br />
-      Likes : <span id="likes">{this.state.likesCount}</span>
-      <div>
-        <button
-          className="beard-button"
-          title="likes stored on mongodb instance"
+        <br />
+        Likes : <span id="likes">{this.state.likesCount}</span>
+        <div>
+          <button
+            className="beard-button"
+            title="likes stored on mongodb instance"
           >
-          <i className="fas fa-thumbs-up" />
-        </button>
+            <i className="fas fa-thumbs-up" />
+          </button>
+        </div>
+        <h3>Like My Beard!</h3>
+        <br />
       </div>
-      <h3>Like My Beard!</h3>
-      <br/>
-    </div>
     );
   }
-};
+}
 
 export default LikeButton;
