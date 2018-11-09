5905696971bb41402e516e3ec1b077b878aa330d
yeti-detective <yeti.detective@gmail.com>
2018-11-09 09:38:14 -0500

    linted Intro.jsx

--- src/client/app/Intro.jsx
+++ src/client/app/Intro.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import LikeButton from './LikeButton'
-import Explaining from './Explaining'
-
+import React from "react";
+import LikeButton from "./LikeButton";
+import Explaining from "./Explaining";
 
 const Intro = () => (
   <main className="intro">
     <LikeButton />
     <Explaining />
   </main>
-)
+);
 
 export default Intro;
